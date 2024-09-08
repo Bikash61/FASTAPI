6acafaf1fd513707b4e33bfdf1d0d6dd0abb0351
Bikash61 <bashyalb96@gmail.com>
2024-09-08 23:26:56 +0530

    Updates

---
+++ secondapi/main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+@app.get("/")
+
+async def root():
+    return {'messages': "Welcome to my API"}
+
+@app.post('/post')    
+def get_post():
+    return {'title': 'My Post', 'content': 'This is my post'}

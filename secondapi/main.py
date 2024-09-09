d8e750316ae179dd90dc39a1518f50238d699585
Bikash61 <bashyalb96@gmail.com>
2024-09-09 22:56:42 +0530

    UPdates

--- secondapi/main.py
+++ secondapi/main.py
@@ -1,11 +1,18 @@
 from fastapi import FastAPI
-
+from fastapi.params import Body
 app = FastAPI()
 @app.get("/")
 
 async def root():
     return {'messages': "Welcome to my API"}
 
-@app.get('/post')    
-def get_post():
+@app.get('/sample')    
+def sample():
     return {'title': 'My Post', 'content': 'This is my post'}
+
+
+
+@app.post('/post')
+async def post(payload : dict = Body(...)):
+    print(payload)
+    return {'newpost': f"Name {payload}"}     
